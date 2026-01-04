39f8eb1e99da3cb38b2b711f652a475077a55d4c
julianftw12 <julianftw12@gmail.com>
2026-01-03 19:39:08 -0800

    Final product :)

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from stats import word_count_func, char_count_func
+from stats import word_count_func, char_count_func, get_integer, sort_dict, print_items
+import sys
 
-with open("books/frankenstein.txt") as f:
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+book_path = sys.argv[1]
+
+with open(f"{book_path}") as f:
     contents = f.read()
 def main():
     word_count_func(contents)
     char_count_func(contents)
+    sort_dict(char_count_func(contents))
+    print(f"""
+    ============ BOOKBOT ============
+    Analyzing book found at {book_path}...
+    ----------- Word Count -----------
+    Found {word_count_func(contents)} total words
+    --------- Character Count -------
+    """)
+    print_items(sort_dict(char_count_func(contents)))
 main()
--- stats.py
+++ stats.py
@@ -1,7 +1,7 @@
 def word_count_func(contents):
     words = contents.split()
     num_words = len(words)
-    print(f"Found {num_words} total words")
+    return num_words
 
 def char_count_func(contents):
     lowercase = contents.lower()
@@ -9,6 +9,22 @@ def char_count_func(contents):
     for char in lowercase:
         if char in chars:
             chars[char] += 1
+        elif char.isalpha() != True:
+            pass
         else:
            chars[char] = 1
-    print(chars)
+    return(chars)
+
+def get_integer(entry):
+    return entry["Integer"]
+
+def sort_dict(original_dict):
+    sorted_list = sorted(
+        [{"Letter" : letter , "Integer" : integer} for letter , integer in original_dict.items()],
+        key = get_integer,
+        reverse = True
+    )
+    return sorted_list
+def print_items(sorted_list):
+    for entry in sorted_list:
+        print(f"{entry['Letter']}: {entry['Integer']}")
